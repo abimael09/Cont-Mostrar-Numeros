08655ca886cda35269ed30aeb0ae37728c1bc74e
Abimael Santos <abymaelsantos@gmail.com>
2023-12-14 18:30:43 -0300

    Criação de projeto em Java contador de 150 à 300

---
+++ src/Cont_Mostrar_Numeros/Cont_Mostrar_Numeros.java
@@ -0,0 +1,18 @@
+
+package Cont_Mostrar_Numeros;
+
+import javax.swing.JOptionPane;
+
+public class Cont_Mostrar_Numeros{
+    
+    public static void main(String[] args){
+        
+        int cont=150;
+        //Mostrando todos os números de 150 a 300.
+        while(cont<=300){
+           System.out.println(cont);
+           cont=cont+1;
+        }
+        
+    }
+}
